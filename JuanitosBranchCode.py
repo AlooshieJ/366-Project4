e4614952277ba35fb926bb657d364b7b3adf11c8
Luis <schaad1@uic.edu>
2019-11-21 20:30:33 -0600

    ADDED A NEW LINE OF CODE FOR THE MACHINE LEARNING ALGO
    Signed-off-by: Luis <schaad1@uic.edu>

--- JuanitosBranchCode.py
+++ JuanitosBranchCode.py
@@ -1,3 +1,11 @@
 print("why cant we just make a bunch of files instead")
 print("hey whats up chiraqui")
 # message written by Ali Jafar : )
+
+Juanito = 'a pussy'
+
+if(Juanito == 'a pussy'):
+    while(1):
+        print(f"chiraqui is {Juanito}")
+
+
--- Luis's Branch.py
+++
@@ -1,7 +0,0 @@
-Luis = 'a pussy'
-
-if(Luis == 'a pussy'):
-    while(1):
-        print(f"Luis is {Luis}")
-
-
