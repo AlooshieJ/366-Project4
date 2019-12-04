07056d872b7db610c8e786ee6e448004e320bbf3
Ali Jafar <ajafar5@uic.edu>
2019-12-04 08:27:53 -0600

    temp push

--- Luis's Branch.py
+++ Luis's Branch.py
@@ -1,1 +1,13 @@
-print("MY WORLD")
+import tkinter as tk
+from tkinter import *
+
+root = tk.Tk()
+
+Canv = tk.createCanvas(400,4000)
+window = tk.create_window(400,400)
+
+window.pack()
+
+
+
+root.mainloop()
