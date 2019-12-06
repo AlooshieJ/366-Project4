74886badc49dc7ba070397a99481f6f13fcab60f
alooshie <43497838+alooshie@users.noreply.github.com>
2019-12-05 19:24:20 -0600

    example of fetch / dictionary usage

--- Main Menu.py
+++ Main Menu.py
@@ -5,98 +5,128 @@ PCControlSignal = {
 "Branch":(0,0,0,0),
 "ALUSrc":(0,0,0,0),
 "RegDst":(0,0,0,0),
-"RegWrite":(0,0,0,0)}
+"RegWrite":(0,0,0,0)
 
-def Fetch (ALL)
-IorD = 0
-AluSrcA = 0
-ALUOp = 00
-PCSrc = 0
-IRWrite = 1
-PCWrite = 1
-
-def Decode (ALL)
-ALUSrcA = 0
-ALUSrcB = 11
-ALUOp = 00
-
-def LSWExectution(self) # (LW, SW)
-ALUSrcA = 1
-ALUSrcB = 10
-ALUOp = 00
-
-def Execution-(R-type)
-ALUSrcA = 1
-ALUSrcB = 00
-ALUOp = 10
-
-def Execution-(BEQ)
-ALUSrcA = 1
-ALUSrcB = 00
-ALUOp = 01
-PCSrc = 1
-Branch = 1
-
-def Exectution - (I-type)
+}
 
 
-def Memory-(LW)
-IorD = 1
-
-def WriteBack-(LW)
-RegDst = 0
-MemtoReg = 1
-RegWrite = 1
-
-def Memory (Addi)
-RegDst = 0
-MemtoReg = 0
-RegWrite = 1
+def fetch(inst=None):
+    print(f"fetch: for {inst}")
+    for sig in PCControlSignal:
+        print(f" {sig}  {PCControlSignal[sig]} ")
 
 
 
-FINAL RESULTS
-----------------------------------#Final output Statistics#--------------------------
 
-Total dynamic instruction count = Total
 
-Total cycle count= Total
+def decode(inst):
+    print(f" decode {inst}")
 
-----------------------------------#Total Cycle Count for#----------------------------
+def execute(inst):
+    print(f" execute {inst}")
 
-3 cycle = Total Count
+tmp = {
 
-4 cycle = Total Count
+    "0": (fetch),
+    "1": (decode),
+    '2': (execute)
 
-5 cycle = Total Count
+}
+for key in tmp:
+   tmp[key]("addi")
 
-----------------------------#value distribution of each control signals#--------------
-MemtoReg:
-0 = Total
-1 = Total
-X = Total
 
-MemWrite:
-0 = Total
-1 = Total
-X = Total
-
-Branch:
-0 = Total
-1 = Total
-X = Total
-
-ALUSrcA:
-0 = Total
-1 = Total
-X = Total
-
-ALUSrcB:
-0 = Total
-1 = Total
-X = Total
-
-RegDst:
-0 = Total
-1 = Total
-X = Total
+#
+# def Fetch (ALL)
+# IorD = 0
+# AluSrcA = 0
+# ALUOp = 00
+# PCSrc = 0
+# IRWrite = 1
+# PCWrite = 1
+#
+# def Decode (ALL)
+# ALUSrcA = 0
+# ALUSrcB = 11
+# ALUOp = 00
+#
+# def LSWExectution(self) # (LW, SW)
+# ALUSrcA = 1
+# ALUSrcB = 10
+# ALUOp = 00
+#
+# def Execution-(R-type)
+# ALUSrcA = 1
+# ALUSrcB = 00
+# ALUOp = 10
+#
+# def Execution-(BEQ)
+# ALUSrcA = 1
+# ALUSrcB = 00
+# ALUOp = 01
+# PCSrc = 1
+# Branch = 1
+#
+# def Exectution - (I-type)
+#
+#
+# def Memory-(LW)
+# IorD = 1
+#
+# def WriteBack-(LW)
+# RegDst = 0
+# MemtoReg = 1
+# RegWrite = 1
+#
+# def Memory (Addi)
+# RegDst = 0
+# MemtoReg = 0
+# RegWrite = 1
+#
+#
+#
+# FINAL RESULTS
+# ----------------------------------#Final output Statistics#--------------------------
+#
+# Total dynamic instruction count = Total
+#
+# Total cycle count= Total
+#
+# ----------------------------------#Total Cycle Count for#----------------------------
+#
+# 3 cycle = Total Count
+#
+# 4 cycle = Total Count
+#
+# 5 cycle = Total Count
+#
+# ----------------------------#value distribution of each control signals#--------------
+# MemtoReg:
+# 0 = Total
+# 1 = Total
+# X = Total
+#
+# MemWrite:
+# 0 = Total
+# 1 = Total
+# X = Total
+#
+# Branch:
+# 0 = Total
+# 1 = Total
+# X = Total
+#
+# ALUSrcA:
+# 0 = Total
+# 1 = Total
+# X = Total
+#
+# ALUSrcB:
+# 0 = Total
+# 1 = Total
+# X = Total
+#
+# RegDst:
+# 0 = Total
+# 1 = Total
+# X = Total
