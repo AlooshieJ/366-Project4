2efbfc6cbfdc6487ff7e2db6d12d23287fbf7853
alooshie <43497838+alooshie@users.noreply.github.com>
2019-12-07 21:34:12 -0600

    new fifo :/
    ordered DIC GARBO

--- fifo.py
+++ fifo.py
@@ -3,44 +3,73 @@ import collections
 class fifo:
     def __init__(self, size):
 
-        self.index = collections.OrderedDict()
+        self._keys = []
+        self._values = []
         self.capacity = size
 
-        #for i in range(size): # creating w/ all size = 0
-         #   self.index.append(i)
+        for i in range(size): # creating w/ all size = 0
+            self._keys.append(i)
+            self._values.append(0)
 
-    def get(self,key):
-        if key in self.index:
-            value = self.index[key]
-            # store value , for removing
-            del self.index[key]
-            #now add it back, already sorting due to structure
-            self.index[key] = value
-            return value
-        else:
+    def pop (self): # removes first element
+
+        del self._keys[0]
+        del self._values[0]
+
+
+
+    def update(self, key, value):
+
+        keyIndex = self._keys.index(key)
+        self._keys[keyIndex] = key
+        self._values[keyIndex] = value
+
+        self.move_to_end(key)
+
+
+    def move_to_end(self,key):
+        keyIndex = self._keys.index(key)
+        value = self._values[keyIndex]
+
+        del self._keys[keyIndex]
+        del self._values[keyIndex]
+
+        self._keys.append(key)
+        self._values.append(value)
+
+    def get(self,key): # returns key value pair
+        try:
+            keyIndex = self._keys.index(key)
+            return self._keys[keyIndex] , self._values[keyIndex]
+        except ValueError:
             return -1
 
-    def set(self,key , value): # set the value of dictionary, given a key
-        if key in self.index:
-            del self.index[key]
-        elif len(self.index) >= self.capacity:
-            self.index.popitem(last= False)
-        self.index[key] = value
 
-    def front(self): # does not update the data structure
-        front = [*self.index.keys()][0] # allows us to acces first index key , aka least used key
-        #print(f" in func{tmp} type: {ty} {tmp2}")
-        return front
 
-    def checkWay(self):
+
+
+    def print(self):
+
+        print(f"keys: {self._keys}\nvalues: {self._values}\n")
+
+    def checkWay(self): # returns *INDEX* of first instance of empty
         occupied = []
         empty = []
-        for key in self.index:
-            if self.index[key] == 0: # empty
-                empty.append(key)
+        keyNum = []
+        key = 0
+        while key < self.capacity:
+            keyIndex = self._keys.index(key)
+            print(f"checking way: {key}", end=" ")
+            if self._values[keyIndex] == 0: # empty
+                empty.append(keyIndex)
+                print(f"---Empty")
+                break
             else:   # occupied
-                empty.append(key)
-        return [empty,occupied]
+                print(f"---Occupied")
+                occupied.append(keyIndex)
+            keyNum.append(key)
+            key += 1
+        return empty,occupied,keyNum
 
 
 
@@ -49,31 +78,21 @@ class fifo:
 
 
 lru = fifo(5)
-for tmp in range(lru.capacity):
-    lru.set(tmp, 0)
-#lru.set(1,0)
+lru.print()
+
+for tmp in range(lru.capacity-3):
+    lru.update(tmp, 1)
+#lru.move_to_end(2)
+lru.print()
+
+lru.update(2,0)
+print(lru.get(2))
+lru.print()
+print(lru.checkWay())
 #for key , value in lru.index:
  #   print( key, value)
-print(lru.index)
-
-#print(lru.get(1))
-print(lru.index[0])
-print(lru.get(2))
-#lru.index.pop()
-print(lru.index)
-
-for i in range(5):
-    if lru.index[i] == 0:
-
-        print("empty, key :",i)
-
-
-for i in range (5):
-    lru.set(i,i)
-    print(lru.index)
-
-    print(f" last used: {lru.front()}")
-
+#print(lru.index)
+lru.print()
 E1 = (40 / 50) * 100
 E2 = (23/34) * 100
 E3 = (35/40) * 100
